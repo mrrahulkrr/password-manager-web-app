58e1a5a9f3243c867f93aba16882fd1f6340769b
root <rahulkrgupta18032003@gmail.com>
2023-10-21 22:25:56 +0000

    passwordManager App

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import passwordManager from './components/passwordManager'
+
+import './App.css'
+
+const App = () => <passwordManager />
+
+export default App
---
+++ src/components/passwordManager/index.js
@@ -0,0 +1,19 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class passwordManager extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <div className="password-manager-bg-container">
+          <img
+            alt="app logo"
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png"
+          />
+        </div>
+      </div>
+    )
+  }
+}
+export default passwordManager
